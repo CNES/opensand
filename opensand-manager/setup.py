8030999ef9a38b72dd32fd508bd303045ca52b79
Julien BERNARD <julien.bernard@toulouse.viveris.com>
2014-12-12 11:06:57 +0100

    Rename sand-shell into sand-server

--- opensand-manager/setup.py
+++ opensand-manager/setup.py
@@ -37,5 +37,5 @@ setup(
                   ('share/opensand/manager/images', img_files),
                   ('share/pixmaps/', icon_files), 
                   ],
-    scripts=['sand-manager','sand-shell']
+    scripts=['sand-manager','sand-server']
 )
