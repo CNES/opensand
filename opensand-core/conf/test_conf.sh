e12c92beb00437ed06f1249ebfe737b318a95530
Julien BERNARD <jbernard@toulouse.viveris.fr>
2015-11-20 11:30:26 +0100

    Update examples configuration files and add tests

--- opensand-core/conf/test_conf.sh
+++ opensand-core/conf/test_conf.sh
@@ -14,13 +14,16 @@ BASEDIR=$( dirname "${SCRIPT}" )
 APP="${BASEDIR}/${APP}"
 
 FILES="core_sat core_gw core_st core_global topology"
+DIRS=`ls examples`
 
+# Test base configurations
 for file in $FILES ; do
 	echo "Testing $file..."
     ${APP} ${BASEDIR}/${file}.conf ${BASEDIR}/xsd/${file}.xsd || exit 1
 	
 done
 
+# Test plugins
 echo "Testing mandatory plugins"
 for file in `ls ${BASEDIR}/mandatory_plugins`; do
     case $file in
@@ -30,4 +33,21 @@ for file in `ls ${BASEDIR}/mandatory_plugins`; do
             ${APP} ${BASEDIR}/mandatory_plugins/${name}.conf ${BASEDIR}/mandatory_plugins/${name}.xsd || exit 1
         ;;
     esac
+done
+
+# Test examples
+for dir in ${DIRS}; do
+    dir="examples/${dir}"
+    echo "Testing GW0 for scenario ${dir}"
+    ${APP} ${dir}/gw0/core.conf ${BASEDIR}/xsd/core_gw.xsd || exit 1
+    echo "Testing SAT for scenario ${dir}"
+    ${APP} ${dir}/sat/core.conf ${BASEDIR}/xsd/core_sat.xsd || exit 1
+    echo "Testing ST1 for scenario ${dir}"
+    ${APP} ${dir}/st1/core.conf ${BASEDIR}/xsd/core_st.xsd || exit 1
+    echo "Testing ST2 for scenario ${dir}"
+    ${APP} ${dir}/st2/core.conf ${BASEDIR}/xsd/core_st.xsd || exit 1
+    echo "Testing Global for scenario ${dir}"
+    ${APP} ${dir}/core_global.conf ${BASEDIR}/xsd/core_global.xsd || exit 1
+    echo "Testing Topology for scenario ${dir}"
+    ${APP} ${dir}/topology.conf ${BASEDIR}/xsd/topology.xsd || exit 1
 done
