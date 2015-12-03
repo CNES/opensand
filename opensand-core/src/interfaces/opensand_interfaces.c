5b69ab05ebfd2df8c60aa38d9b36e2aa8100b978
Julien BERNARD <jbernard@toulouse.viveris.fr>
2015-12-03 15:27:29 +0100

    Fix Issue #3: https://forge.net4sat.org/opensand/opensand/issues/3
    opensand-daemon installation bug "Failed to set bridge down: No such device"

--- opensand-core/src/interfaces/opensand_interfaces.c
+++ opensand-core/src/interfaces/opensand_interfaces.c
@@ -120,7 +120,7 @@ static void del_bridge()
 	{
 		fprintf(stderr, "Failed to set bridge down: %s\n", strerror(errno));
 	}
-	printf("Deleting bridge");
+	printf("Deleting bridge\n");
 	err = br_del_bridge(br);
 	if(err)
 	{
@@ -205,9 +205,9 @@ int bridge(int delete)
 	}
 
 	// remove bridge if it already exists or if we want to delete interfaces
-	del_bridge();
 	if(delete)
 	{
+		del_bridge();
 		br_shutdown();
 		return 0;
 	}
