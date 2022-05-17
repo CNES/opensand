826a64cf11009bc8d66dfac45f1f9b31c3e20866
Yohan Simard <yohan.simard@viveris.fr>
2022-05-17 13:34:16 +0200

    Add license header in NetPacketSerializer

--- opensand-core/src/mesh/NetPacketSerializer.h
+++ opensand-core/src/mesh/NetPacketSerializer.h
@@ -1,3 +1,40 @@
+/*
+ *
+ * OpenSAND is an emulation testbed aiming to represent in a cost effective way a
+ * satellite telecommunication system for research and engineering activities.
+ *
+ *
+ * Copyright © 2019 TAS
+ * Copyright © 2019 CNES
+ *
+ *
+ * This file is part of the OpenSAND testbed.
+ *
+ *
+ * OpenSAND is free software : you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY, without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+/**
+ * @file NetPacketSerializer.h
+ * @brief This files defines a struct that serializes a NetPacket
+ * @author Yohan Simard <yohan.simard@viveris.fr>
+ */
+
+#ifndef NET_PACKET_SERIALIZER_H
+#define NET_PACKET_SERIALIZER_H
+
 #include "NetPacket.h"
 #include <array>
 #include <cstdint>
@@ -22,3 +59,5 @@ struct __attribute__((__packed__)) net_packet_buffer_t
 	net_packet_buffer_t(const NetPacket &pkt);
 	std::unique_ptr<NetPacket> deserialize() const;
 };
+
+#endif
