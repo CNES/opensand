f1d3148fe16333e735f3f2c0f1696e5024b082cb
Mathias Ettinger <mathias.ettinger@viveris.fr>
2022-06-09 15:35:55 +0200

    Improve opensand-network scripts

--- opensand-network/opensand_network/opensand_netns.py
+++ opensand-network/opensand_network/opensand_netns.py
@@ -1,89 +1,209 @@
 #!/bin/python3
 
-import sys
-from opensand_network_utils import *
-
-dev_bridge = "dev_bridge"
-emu_bridge = "emu_bridge"
-
-# IDs of the entities to configure
-gateways = [1, 2]
-terminals = [11, 12]
-satellites = [21, 22]
+"""Setup netns for OpenSAND emulator"""
 
 
-def create_entity(type, id, create_tap):
-    name = f"{type}{id}"
-    create_netns(name)
-    set_up("lo", name)
-    
-    # dev network
-    veth_dev_remote = f"dev_{name}"
-    veth_dev_local = f"dev_lo_{name}"
-    create_veth_ifaces_pair(veth_dev_remote, veth_dev_local)
-    move_iface(veth_dev_remote, name)
-    add_address(veth_dev_remote, f"192.168.100.{id}/24", name)
-    attach_iface(dev_bridge, veth_dev_local)
-    set_up(veth_dev_local)
-    set_up(veth_dev_remote, name)
+import os
+import re
+import typing
+import argparse
+import subprocess
 
-    # emu network
-    veth_emu_remote = f"emu_{name}"
-    veth_emu_local = f"emu_lo_{name}"
-    create_veth_ifaces_pair(veth_emu_remote, veth_emu_local)
-    move_iface(veth_emu_remote, name)
-    add_address(veth_emu_remote, f"192.168.18.{id}/24", name)
-    attach_iface(emu_bridge, veth_emu_local)
-    set_up(veth_emu_local)
-    set_up(veth_emu_remote, name)
-    
+import opensand_network_utils as utils
+
+
+IDs = typing.Iterable[int]
+File = typing.Union[str, typing.IO]
+
+
+def create_network(tag: str, name: str, address: str, bridge: str):
+    veth_remote = f'{tag}_{name}'
+    veth_local = f'{tag}_lo_{name}'
+    utils.create_veth_ifaces_pair(veth_remote, veth_local)
+    utils.move_iface(veth_remote, name)
+    utils.add_address(veth_remote, address, name)
+    utils.attach_iface(bridge, veth_local)
+    utils.set_up(veth_local)
+    utils.set_up(veth_remote, name)
+
+
+def create_entity(entity: str, entity_id: int, dev_bridge: str, emu_bridge: str, create_tap: bool):
+    name = f'{entity}{entity_id}'
+    utils.create_netns(name)
+    utils.set_up('lo', name)
+    create_network('dev', name, f'192.168.100.{entity_id}/24', dev_bridge)
+    create_network('emu', name, f'192.168.18.{entity_id}/24', emu_bridge)
+
     if create_tap:
-        tap_interface = "opensand_tap"
-        create_tap_iface(tap_interface, name)
-        set_mac_address(tap_interface, f"00:00:00:00:00:{id:0>2}", name)
-        add_address(tap_interface, f"192.168.63.{id}/24", name)
-        set_up(tap_interface, name)
+        tap_interface = 'opensand_tap'
+        tap_mac = f'00:00:00:00:00:{entity_id:0>2}'
+        tap_address = f'192.168.63.{entity_id}/24'
+        utils.create_tap_iface(tap_interface, name)
+        utils.set_mac_address(tap_interface, tap_mac, name)
+        utils.add_address(tap_interface, tap_address, name)
+        utils.set_up(tap_interface, name)
 
 
-def setup():
-    create_bridge(dev_bridge, [])
-    create_bridge(emu_bridge, [])
+def setup(terminals: IDs, gateways: IDs, satellites: IDs, dev_bridge: str, emu_bridge: str):
+    utils.create_bridge(dev_bridge, [])
+    utils.create_bridge(emu_bridge, [])
     for id in terminals:
-        create_entity("st", id, True)
+        create_entity('st', id, dev_bridge, emu_bridge, True)
     for id in gateways:
-        create_entity("gw", id, True)
+        create_entity('gw', id, dev_bridge, emu_bridge, True)
     for id in satellites:
-        create_entity("sat", id, False)
-    
-    add_address(dev_bridge, "192.168.100.100/24")
-    set_up(dev_bridge)
-    set_up(emu_bridge)
+        create_entity('sat', id, dev_bridge, emu_bridge, False)
 
-def clean():
-    set_stop_on_error(False)
-    for id in terminals:
-        delete_netns(f"st{id}")
-    for id in gateways:
-        delete_netns(f"gw{id}")
-    for id in satellites:
-        delete_netns(f"sat{id}")
-
-    delete_iface("dev_bridge")
-    delete_iface("emu_bridge")
-    set_stop_on_error(True)
+    utils.add_address(dev_bridge, '192.168.100.100/24')
+    utils.set_up(dev_bridge)
+    utils.set_up(emu_bridge)
 
 
-def print_usage_and_exit(exit_code):
-    print("Usage: opensand_netns.py <up|down>")
-    sys.exit(exit_code)
+def clean(dev_bridge: str, emu_bridge: str):
+    utils.RAISE_ERROR = False
+
+    pattern = re.compile(r'((st|gw|sat)\d+)(\s|$)')
+    for netns in utils.list_netns():
+        match = pattern.match(netns)
+        if match is not None:
+            utils.delete_netns(match.group(1))
+
+    utils.delete_iface(dev_bridge)
+    utils.delete_iface(emu_bridge)
+
+    utils.RAISE_ERROR = True
+
+
+def _extract_file_name(filepath: File) -> str:
+    try:
+        with filepath:
+            return filepath.name
+    except AttributeError:
+        pass
+
+    return filepath
+
+
+def launch(entity_kind: str, entity_id: int, topology: File, infrastructure: File, profile: File=None):
+    topology = _extract_file_name(topology)
+    infrastructure = _extract_file_name(infrastructure)
+    profile = _extract_file_name(profile)
+
+    command = ['ip', 'netns', 'exec', f'{entity_kind}{entity_id}']
+
+    sudo_user = os.getenv('SUDO_USER')
+    if sudo_user:
+        command.extend(('sudo', '-E', '-u', sudo_user))
+
+    command.extend(('opensand', '-v', '-i', infrastructure, '-t', topology))
+    if profile is not None:
+        command.extend(('-p', profile))
+
+    subprocess.run(command, check=True)
+
+
+def unsigned_byte(value: str) -> int:
+    value = int(value)
+    if value < 0:
+        raise ValueError(f'invalid negative value: {value}')
+    if value > 0xFF:
+        raise ValueError(f'invalid non-byte value: {value}')
+    return value
+
+
+def command_line_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description=__doc__, fromfile_prefix_chars='@')
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+    subparsers = parser.add_subparsers(title='Mode', help='Mode of operation')
+    subparsers.required = True
+
+    parser_clean = subparsers.add_parser('clean', help='Remove netns that were previously created')
+    parser_clean.add_argument(
+            '-d', '--dev', '--dev-bridge',
+            dest='dev_bridge',
+            metavar='NAME',
+            default='dev_bridge',
+            help='Name of the dev bridge')
+    parser_clean.add_argument(
+            '-e', '--emu', '--emu-bridge',
+            dest='emu_bridge',
+            metavar='NAME',
+            default='emu_bridge',
+            help='Name of the emu bridge')
+
+    parser_setup = subparsers.add_parser('setup', help='Create netns to run OpenSAND in')
+    parser_setup.add_argument(
+            '-d', '--dev', '--dev-bridge',
+            dest='dev_bridge',
+            metavar='NAME',
+            default='dev_bridge',
+            help='Name of the dev bridge')
+    parser_setup.add_argument(
+            '-e', '--emu', '--emu-bridge',
+            dest='emu_bridge',
+            metavar='NAME',
+            default='emu_bridge',
+            help='Name of the emu bridge')
+    parser_setup.add_argument(
+            '-t', '--st', '--terminal',
+            dest='terminals',
+            metavar='ID',
+            default=(),
+            type=unsigned_byte,
+            nargs='+',
+            help='IDs of terminals to create netns for')
+    parser_setup.add_argument(
+            '-g', '--gw', '--gateway',
+            dest='gateways',
+            metavar='ID',
+            default=(),
+            type=unsigned_byte,
+            nargs='+',
+            help='IDs of gateways to create netns for')
+    parser_setup.add_argument(
+            '-s', '--sat', '--satellite',
+            dest='satellites',
+            metavar='ID',
+            default=(),
+            type=unsigned_byte,
+            nargs='+',
+            help='IDs of satellites to create netns for')
+
+    parser_launch = subparsers.add_parser('launch', help='Run OpenSAND inside a netns')
+    parser_launch.add_argument(
+            'entity_kind',
+            choices=('st', 'gw', 'sat'),
+            help='Type of the entity that will be launched')
+    parser_launch.add_argument(
+            'entity_id',
+            metavar='ID',
+            type=unsigned_byte,
+            help='ID of the entity that will be launched')
+    parser_launch.add_argument(
+            'topology',
+            type=argparse.FileType('r'),
+            help='Path to the topology.xml file for this entity')
+    parser_launch.add_argument(
+            'infrastructure',
+            type=argparse.FileType('r'),
+            help='Path to the infrastructure.xml file for this entity')
+    parser_launch.add_argument(
+            'profile',
+            type=argparse.FileType('r'),
+            nargs='?',
+            help='Path to the profile.xml file for this entity')
+
+    # Set subparsers options to automatically call the right
+    # function depending on the chosen subcommand
+    parser_clean.set_defaults(function=clean)
+    parser_setup.set_defaults(function=setup)
+    parser_launch.set_defaults(function=launch)
+
+    return parser
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print_usage_and_exit(1)
-    if sys.argv[1] == "up":
-        setup()
-    elif sys.argv[1] == "down":
-        clean()
-    else:
-        print_usage_and_exit(2)
+    args = vars(command_line_parser().parse_args())
+    utils.VERBOSITY = args.pop('verbose')
+    main = args.pop('function')
+    main(**args)
