997dd8999a1b40319ecab12eba9d0cce2057e6b0
Mathias Ettinger <mathias.ettinger@viveris.fr>
2022-06-20 11:33:41 +0200

    Fix transparent block

--- opensand-core/src/mesh/BlockTransp.cpp
+++ opensand-core/src/mesh/BlockTransp.cpp
@@ -54,7 +54,7 @@ bool BlockTransp::Upward::onEvent(const RtEvent *const event)
 
 	auto msg_event = static_cast<const MessageEvent *>(event);
 
-	if (to_enum<InternalMessageType>(msg_event->getMessageType()) != InternalMessageType::msg_data)
+	if (to_enum<InternalMessageType>(msg_event->getMessageType()) != InternalMessageType::msg_unknown)
 	{
 		LOG(log_receive, LEVEL_ERROR, "Unexpected message received: %s",
 		    msg_event->getName().c_str());
